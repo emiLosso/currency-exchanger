f0b22d373b34a88ff8060d7621e840a0e0949001
Emiliano Losso <emilianolosso@gmail.com>
2018-03-15 13:36:35 -0300

    new client project with angular5

--- exchanger_app/views.py
+++ exchanger_app/views.py
@@ -6,8 +6,6 @@ from serializers import UserSerializer, CurrencySerializer, WalletSerializer, Tr
 
 from rest_framework import permissions
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route, list_route
-from rest_framework.response import Response
 from django.views import generic
 from django.shortcuts import render
 
@@ -31,18 +29,6 @@ class CurrencyViewSet(viewsets.ModelViewSet):
     serializer_class = CurrencySerializer
     permission_classes = (permissions.IsAuthenticated,)
 
-    @list_route()
-    def havent_user(self, request):
-        user_currencies = Wallet.objects.filter(
-            user=request.user).values_list('currency', flat=True)
-        currencies = Currency.objects.exclude(pk__in=user_currencies)
-
-        if currencies:
-            serializer = self.get_serializer(currencies, many=True)
-            return Response(serializer.data)
-
-        return Response([])
-
 
 class WalletViewSet(viewsets.ModelViewSet):
     queryset = Wallet.objects.all()
@@ -52,20 +38,6 @@ class WalletViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         return Wallet.objects.filter(user=user)
-
-    @detail_route(methods=['post'])
-    def get_wallet_of_user(self, request, pk=None):
-        user = User.objects.get(pk=pk)
-        wallets_of = Wallet.objects.filter(
-            user=user).filter(currency=request.data['id'])
-
-        if wallets_of:
-            serializer = self.get_serializer(wallets_of, many=True)
-            return Response(serializer.data)
-
-        return Response(
-            {'detail': 'The user has not wallet a of this currency'},
-            status=400)
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
