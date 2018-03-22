37f5a5c17812b18bc67d043e7746357ee3a63222
Emiliano Losso <emilianolosso@gmail.com>
2018-03-21 22:26:37 -0300

    test transaction operation

--- exchanger_app/tests.py
+++ exchanger_app/tests.py
@@ -6,8 +6,9 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from django.core.urlresolvers import reverse
 
-from models import Currency
+from models import Currency, Wallet
 from django.contrib.auth.models import User
+import datetime
 
 
 class ApiTestCase(APITestCase):
@@ -26,17 +27,27 @@ class ApiTestCase(APITestCase):
         }
         self.client = APIClient()
         self.client.login(username=self.username, password=self.password)
-        self.create_currency()
+        self.currency = self.create_currency("Bitcoin", "B")
 
-    def create_currency(self):
+    def create_currency(self, name, sign):
         c = Currency()
-        c.name = "Bitcoin"
-        c.sign = 'B'
+        c.name = name
+        c.sign = sign
         c.save()
+
+    def create_wallet(self, currency, user, balance):
+        w = Wallet()
+        w.currency = currency
+        w.user = user
+        w.balance = balance
+
+    def create_user(self, email, user, password):
+        User.objects.create_user(
+            user, email, password)
 
     def test_api_can_create_a_currency(self):
         """Test the api has currency creation capability."""
-        self.currency_data = {'name': 'Dogecoin', 'sign': 'D'}
+        self.currency_data = {'name': 'New Currency', 'sign': 'N'}
         self.response = self.client.post(
             reverse('api:currencies-list'),
             self.currency_data,
@@ -73,3 +84,60 @@ class ApiTestCase(APITestCase):
             follow=True)
 
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_api_can_create_a_transaction(self):
+        """Test the api has transaction creation capability."""
+        user1 = User.objects.create_user("user1", "user1@user.com", "admin123")
+        user2 = User.objects.create_user("user2", "user2@user.com", "admin123")
+        currency1 = Currency.objects.create(
+            name="Dogecoin",
+            sign="D",
+        )
+        wallet1 = Wallet.objects.create(
+            currency=currency1,
+            user=user1,
+            balance="1000",
+        )
+        wallet2 = Wallet.objects.create(
+            currency=currency1,
+            user=user2,
+            balance="1000",
+        )
+
+        self.transaction_data = {
+            "amount": 20,
+            "date_time": datetime.datetime.now(),
+            "origin": {
+                "id": wallet1.pk,
+                "balance": wallet1.balance,
+                "currency": {
+                    "id": currency1.pk,
+                    "name": currency1.name,
+                    "sign": currency1.sign
+                },
+                "user": {
+                    "id": user1.pk,
+                    "username": user1.username
+                }
+            },
+            "destination": {
+                "id": wallet2.pk,
+                "balance": wallet2.balance,
+                "currency": {
+                    "id": currency1.pk,
+                    "name": currency1.name,
+                    "sign": currency1.sign
+                },
+                "user": {
+                    "id": user2.pk,
+                    "username": user2.username
+                }
+            }
+        }
+
+        self.response = self.client.post(
+            reverse('api:transactions-list'),
+            self.transaction_data,
+            format="json",
+        )
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- exchanger_app/urls.py
+++ exchanger_app/urls.py
@@ -2,18 +2,11 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from exchanger_app import views
 
-
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'wallets', views.WalletViewSet)
-router.register(r'transactions', views.TransactionViewSet)
+router.register(r'transactions', views.TransactionViewSet, base_name="transactions")
 router.register(r'currencies', views.CurrencyViewSet, base_name="currencies")
-
-
-# urlpatterns = [
-#     url(r'^api/', include(router.urls)),
-# ]
-
 
 urlpatterns = [
     url(r'^api/', include(router.urls,
